fc63f36b555c61cf16703f8c7f327983df3b0d15
DeliriumTremens <egonzalez@geomar.de>
2019-09-06 17:28:16 +0200

    my final commit

--- html/js/index.js
+++ html/js/index.js
@@ -4,8 +4,13 @@ window.addEventListener("load", function(){
     //var corner1 = L.latLng(51,1);
     //var corner2 = L.latLng(60, 8);
     
-    var corner1 = L.latLng(51,1.5);
-    var corner2 = L.latLng(53, 3.5);
+    var startLat = 51;
+    var endLat = 53;
+    var startLon = 1.5;
+    var endLon = 3.5;
+    
+    var corner1 = L.latLng(startLat,startLon);
+    var corner2 = L.latLng(endLat, endLon);
     
     var bounds = L.latLngBounds(corner1, corner2);
     
@@ -54,16 +59,32 @@ window.addEventListener("load", function(){
         });
     
     //---------Plot polygons--------------
-    
-    var polygon = L.polygon([
-	   [51.552125, 1.516433],
-	   [51.552125, 1.839815],
-	   [51.434955, 1.839815],
-	   [51.434955, 1.516433],
-        ],{color: 'red',weight: '1'}).addTo(mymap);
+     d3.csv('resources/boxes.csv').then(function(data){
+         
+         for(var ii=0; ii<1000; ii++){
+             
         
-    polygon.bindPopup("<b>Vulnerable Zone</b><br>ICES VMEDataSet201994");
- 
+//             if(data[ii].StartLatitude>0 &&
+//                data[ii].StartLongitude>-180 && 
+//                data[ii].EndLatitude<90 && 
+//                data[ii].EndLlongitude<360
+//               ){
+         
+                var polygon = L.polygon([
+                   [data[ii].StartLatitude, data[ii].StartLongitude],
+                   [data[ii].StartLatitude, data[ii].EndLongitude],
+                   [data[ii].EndLatitude, data[ii].EndLongitude],
+                   [data[ii].EndLatitude, data[ii].StartLongitude],
+                    ],{color: 'red',weight: '1'}).addTo(mymap);
+
+                
+             
+                polygon.bindPopup("<b>Vulnerable Zone</b><br>ICES VMEDataSet201994");
+                
+             //}
+            }
+         });
+
     //---------Plot Heatmaps--------------
    
     //d3.tsv('resources/waves.csv').then(function(data){
@@ -72,12 +93,11 @@ window.addEventListener("load", function(){
         let keys = Object.keys(data[0]);    
         var heatmap = []
         var scale = 10;  
-        console.log(data)
         for(var ii = 0; ii<data.length; ii++){
     
                 heatmap.push([data[ii][keys[0]], data[ii][keys[1]],data[ii][keys[2]]*scale]);
             
-             //   heatmap.push([data[ii][keys[1]], data[ii][keys[2]],data[ii][keys[0]]*scale]); // waves
+                //heatmap.push([data[ii][keys[1]], data[ii][keys[2]],data[ii][keys[0]]*scale]); // waves
         }
         
         var heat = L.heatLayer(heatmap, 
@@ -102,23 +122,27 @@ window.addEventListener("load", function(){
 //        });
 //    
 //    
-    var sensorTab = new Vue({
     
-    el:"#sensor-list",
     
-    data: {
- 
-    },
-    
-    methods:{
-    
-    },
-})
-
-    
-    console.log("dat workd jo!")            
-
-
+//    var sensorTab = new Vue({
+//    
+//    el:"#sensor-list",
+//    
+//    data: {
+// 
+//    },
+//    
+//    methods:{
+//    
+//    },
+//})
+//
+//    
+//    console.log("dat workd jo!")            
+//
+//
 });
 
-
+Number.prototype.mod = function(n) {
+    return ((this%n)+n)%n;
+};
